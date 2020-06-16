deb80301141c9ff7e1d4f7d6f373545081fb15b2
Flashky <mevengar@gmail.com>
2020-06-16 16:24:47 +0200

    Added projectiles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Cactus : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
+    [SerializeField] float speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+
+    GameObject gun;
+
+    private void Start()
+    {
+        gun = transform.GetChild(0).gameObject;
+    }
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
