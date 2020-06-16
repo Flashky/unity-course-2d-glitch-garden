59f24bee24b33f2eb3b100b9c894604512278c15
Flashky <mevengar@gmail.com>
2020-06-16 15:02:43 +0200

    Created Cactus defender

--- Assets/Scripts/Cactus.cs
+++ Assets/Scripts/Cactus.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour
+public class Cactus : MonoBehaviour
 {
-    float currentSpeed = 1f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +13,6 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);  
-    }
-
-    public void SetMovementSpeed(float speed)
-    {
-        currentSpeed = speed;
+        
     }
 }
