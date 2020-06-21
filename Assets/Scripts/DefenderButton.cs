98e23625736828dd121d05cd0f7b0d8aced88f07
Flashky <mevengar@gmail.com>
2020-06-21 21:27:30 +0200

    Added defender selection buttons

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    [SerializeField] Color32 unselectedColor;
+
+    SpriteRenderer sprite;
+
+    
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();    
+    }
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = unselectedColor;
+        }
+
+        sprite.color = Color.white;
+    }
+
+}
